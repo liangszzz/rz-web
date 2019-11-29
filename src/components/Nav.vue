413c4fb165311e0a232ba6e58d543d9354965600
liangshan <liang-shan@outlook.com>
2019-11-29 11:01:21 +0800

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="nav">
+    <h1>{{ msg }}</h1>
+</div>
+</template>
+
+<script lang="ts">
+import {
+    Component,
+    Prop,
+    Vue
+} from 'vue-property-decorator';
+
+@Component
+export default class Nav extends Vue {
+    @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+.nav {
+    float: left;
+}
+</style>
